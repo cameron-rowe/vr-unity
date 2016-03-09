cc1895d84438104e470e9b6ebe6d3b67c06aa02f
Cameron Rowe <crowe@nevada.unr.edu>
2016-03-09 13:52:15 -0800

    initial commit

---
+++ Assets/Config.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+using NLua;
+
+public delegate bool ButtonCallback(int channel);
+public delegate float AnalogCallback(int channel);
+public delegate Sixdof SixdofCallback(int channel);
+
+public class Config : MonoBehaviour
+{
+    private Lua state = new Lua();
+
+    private Dictionary<string, ButtonCallback> buttonCallbacks = new Dictionary<string, ButtonCallback>();
+    private Dictionary<string, AnalogCallback> analogCallbacks = new Dictionary<string, AnalogCallback>();
+    private Dictionary<string, SixdofCallback> sixdofCallbacks = new Dictionary<string, SixdofCallback>();
+
+    void Start() {
+        state.DoString(string.Format("HOSTNAME = '{0}'", System.Environment.MachineName));
+
+        Debug.Log(string.Format("HOSTNAME = '{0}'", state["HOSTNAME"]));
+
+        //System.IO.File.WriteAllText("log.txt", string.Format("{0}  {1}  {2}  {3}", Application.dataPath, Application.persistentDataPath, Application.streamingAssetsPath, Application.temporaryCachePath));
+        state.DoFile(Application.streamingAssetsPath + "/example.lua");
+
+        var vrpn = state["vrpn"] as LuaTable;
+
+        //System.IO.File.WriteAllText("log2.txt", string.Format("{0}  {1}", State["HOSTNAME"], vrpn));
+
+        var buttons = vrpn["buttons"] as LuaTable;
+        var analogs = vrpn["analogs"] as LuaTable;
+        var sixdofs = vrpn["sixdofs"] as LuaTable;
+
+        if(buttons != null) {
+            foreach(var b in buttons.Values) {
+                var button = b.ToString();
+                Debug.Log("Button: " + button);
+                var device = button.Split('@')[0];
+
+                ButtonCallback callback = (int channel) => {
+                    return VRPN.vrpnButton(button, channel);
+                };
+
+                buttonCallbacks.Add(device, callback);
+            }
+        }
+
+        if(analogs != null) {
+            foreach(var a in analogs.Values) {
+                var analog = a.ToString();
+                Debug.Log("Analog: " + analog);
+
+                var device = analog.Split('@')[0];
+
+                AnalogCallback callback = (int channel) => {
+                    return (float)VRPN.vrpnAnalog(analog, channel);
+                };
+
+                analogCallbacks.Add(device, callback);
+            }
+        }
+
+        if(sixdofs != null) {
+            foreach(var s in sixdofs.Values) {
+                var sixdof = s.ToString();
+                Debug.Log("Sixdof: " + sixdof);
+
+                var device = sixdof.Split('@')[0];
+
+                SixdofCallback callback = (int channel) => {
+                    var pos = VRPN.vrpnTrackerPos(sixdof, channel);
+                    var rot = VRPN.vrpnTrackerQuat(sixdof, channel);
+
+                    pos.z *= -1;
+
+                    return new Sixdof {
+                        Position = pos,//new Vector3(pos.x, pos.y, -pos.z),
+                        Rotation = Quaternion.Inverse(rot),//new Quaternion(-rot.x, rot.z, rot.y, rot.w)
+                    };
+                };
+
+                sixdofCallbacks.Add(device, callback);
+            }
+        }
+    }
+
+    public bool GetButtonValue(string device, int channel) {
+        return buttonCallbacks[device](channel);
+    }
+
+    public float GetAnalogValue(string device, int channel) {
+        return analogCallbacks[device](channel);
+    }
+
+    public Sixdof GetSixdofValue(string device, int channel) {
+        return sixdofCallbacks[device](channel);
+    }
+}
---
+++ Assets/VRPNInputManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class VRPNInputManager : MonoBehaviour
+{
+    public Dictionary<string, string> ButtonMap = new Dictionary<string, string>();
+    public Dictionary<string, string> AnalogMap = new Dictionary<string, string>();
+    public Dictionary<string, string> SixdofMap = new Dictionary<string, string>();
+
+    public Dictionary<string, bool> ButtonValues = new Dictionary<string, bool>();
+    public Dictionary<string, float> AnalogValues = new Dictionary<string, float>();
+    public Dictionary<string, Sixdof> SixdofValues = new Dictionary<string, Sixdof>();
+
+    private Config config;
+    // Use this for initialization
+    void Start() {
+
+        ButtonMap.Add("clear", "WiiMote0[0]"); // Home button
+        ButtonMap.Add("paint", "WiiMote0[3]"); // A button
+        ButtonMap.Add("rotate", "WiiMote0[16]"); // nunchuck-z button
+        ButtonMap.Add("boost", "WiiMote0[17]"); // nunchuck-c button
+
+        AnalogMap.Add("x", "WiiMote0[21]"); // nunchuck-y
+        AnalogMap.Add("z", "WiiMote0[22]"); // nunchuck-z
+
+        SixdofMap.Add("wand", "WiiMote0[0]"); // WiiMote0
+        SixdofMap.Add("view", "ShortGlasses[0]"); // short glasses
+        
+        foreach(var button in ButtonMap.Values) {
+            ButtonValues.Add(button, false);
+        }
+
+        foreach(var analog in AnalogMap.Values) {
+            AnalogValues.Add(analog, 0.0f);
+        }
+
+        foreach(var sixdof in SixdofMap.Values) {
+            SixdofValues.Add(sixdof, new Sixdof());
+        }
+
+        config = FindObjectOfType<Config>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        string device;
+        int channel;
+
+        foreach(var button in ButtonMap.Values) {
+            GetDeviceAndChannel(button, out device, out channel);
+            ButtonValues[button] = config.GetButtonValue(device, channel);
+        }
+
+        foreach(var analog in AnalogMap.Values) {
+            GetDeviceAndChannel(analog, out device, out channel);
+            AnalogValues[analog] = config.GetAnalogValue(device, channel);
+        }
+
+        foreach(var sixdof in SixdofMap.Values) {
+            GetDeviceAndChannel(sixdof, out device, out channel);
+            SixdofValues[sixdof] = config.GetSixdofValue(device, channel);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+
+    public bool GetButtonValue(string name) {
+        return ButtonValues[ButtonMap[name]];
+    }
+
+    public float GetAnalogValue(string name) {
+        return AnalogValues[AnalogMap[name]];
+    }
+
+    public Sixdof GetSixdofValue(string name) {
+        return SixdofValues[SixdofMap[name]];
+    }
+
+    private static void GetDeviceAndChannel(string addr, out string device, out int channel) {
+        var split = addr.Split('[');
+        device = split[0].Replace("]", string.Empty);
+        var channelStr = split[1].Replace("]", string.Empty);
+        channel = int.Parse(channelStr);
+    }
+}
