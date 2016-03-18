20fc1957c895ec7497218852f0a4126a3eccefac
Cameron Rowe <crowe@nevada.unr.edu>
2016-03-18 10:20:21 -0700

    added ssh

--- Assets/CaVR.cs
+++ Assets/CaVR.cs
@@ -20,37 +20,42 @@ public class CaVR : MonoBehaviour {
 		inputManger = FindObjectOfType<VRPNInputManager>();
 
 		if(config.IsMaster) {
-			var machines = config.GetLuaStateValue("machines") as LuaTable;
-			foreach(var machineKey in machines.Keys) {
-				var machine = machineKey.ToString();
-				if(machine == "master") {
-					continue;
-				}
+            if(Environment.OSVersion.Platform == PlatformID.MacOSX || Environment.OSVersion.Platform == PlatformID.MacOSX) { 
+			    var machines = config.GetLuaStateValue("machines") as LuaTable;
+                foreach(var machineKey in machines.Keys) {
+                    var machine = machineKey.ToString();
+                    if(machine == "master") {
+                        continue;
+                    }
 
-				var ssh = machines[machineKey + ".ssh"].ToString();
+                    var ssh = machines[machineKey + ".ssh"].ToString();
 
-				var cwd = Environment.CurrentDirectory;
+                    var cwd = Environment.CurrentDirectory;
 
-				var command = new StringBuilder();
-				command.Append("cd ").Append(cwd).Append(" &&");
+                    var command = new StringBuilder();
+                    command.Append("cd ").Append(cwd).Append(" &&");
 
-				foreach(var arg in Environment.GetCommandLineArgs()) {
-					command.Append(' ');
-					command.Append(arg);
-				}
+                    foreach(var arg in Environment.GetCommandLineArgs()) {
+                        command.Append(' ');
+                        command.Append(arg);
+                    }
 
-				command.Append(" --cavr_master=").Append(config.GetLuaStateValue("machines.master.address").ToString());
+                    command.Append(" --cavr_master=").Append(config.GetLuaStateValue("machines.master.address").ToString());
 
-				var sshOptions = new ProcessStartInfo("ssh", command.ToString());
-				var process = new Process();
-				process.StartInfo = sshOptions;
+                    var sshOptions = new ProcessStartInfo("ssh", command.ToString());
+                    var process = new Process();
+                    process.StartInfo = sshOptions;
 
-				if(!process.Start()) {
-					UnityEngine.Debug.LogError("Unable to launch SSH");
-				}
+                    if(!process.Start()) {
+                        UnityEngine.Debug.LogError("Unable to launch SSH");
+                    }
 
-				Thread.Sleep(1000 * 10);
+                    Thread.Sleep(1000 * 10);
+                }
 			}
+            else {
+                UnityEngine.Debug.LogWarning("Windows currently not supported for cross network CaVR");
+            }
 		}
 
 		networkThread = new Thread(() => {
